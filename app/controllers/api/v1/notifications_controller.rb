4a6bd52da60600a9809ae38c196faec1d8cf7faf
Ueverton-oliveira <ueverton.souz@gmail.com>
2024-08-07 14:51:20 -0400

    fix: notification controller

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -1,10 +1,9 @@
 class Api::V1::NotificationsController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
   def create
     notification = Notification.new(notification_params)
+
     if notification.save
-      render json: { message: 'Notification received successfully' }, status: :ok
+      render json: { message: 'Notification received' }, status: :ok
     else
       render json: { error: 'Failed to save notification' }, status: :unprocessable_entity
     end
@@ -13,6 +12,6 @@ class Api::V1::NotificationsController < ApplicationController
   private
 
   def notification_params
-    params.require(:notification).permit(:task_id, :task_title, :task_description, :user_id, :action)
+    params.require(:notification).permit(:message, :received_at, :task_details, :user_data)
   end
 end
