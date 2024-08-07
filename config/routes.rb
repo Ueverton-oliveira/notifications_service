a8fc1f1ffdf60b30a0d78e2e8f0c1a65b8d02098
Ueverton-oliveira <ueverton.souz@gmail.com>
2024-08-07 14:52:21 -0400

    fix: routas to notification

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      post 'notifications', to: 'notifications#create'
+      resources :notifications, only: [:create]
     end
   end
 end
